39dcb8b9adb1f47f581048af22b12f882e4f0dbb
Manurla/Manu <92942353+manusear1@users.noreply.github.com>
2023-02-17 10:52:55 -0300

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover">
+  <meta http-equiv="Content-Security-Policy" content="default-src * data: gap: https://ssl.gstatic.com; style-src * 'unsafe-inline'; script-src * 'unsafe-inline' 'unsafe-eval'">
+  <link rel="stylesheet" href="arrumando.css">
+
+</head>
+<body>
+
+    <div class="vermelho">
+        <p> </p>
+    </div>
+     <div class="preto">
+        <p> </p>
+    </div>
+    <h2> LOGIN </h2>
+    <div id="divLoagin">
+        <form id="entrar" method="post">
+            <input type="email" id="login" name="email" placeholder="Email..."/> <br> <br>
+            <input type="password" id="senha" name="senha" placeholder="Senha..."/> <br>
+            <br> <button class="bn632-hover bn22" id="btn"> Entrar </button>
+        </form>
+        
+    </div>
+
+    <br> <h3> Server: </h3>  <input id="login" type="url" name="url"><br> <br>
+    
+     <h2> UPLOAD </h2>
+    <h3>Arquivo a enviar:</h3> <input id="Myfile" type="file" name="Escolher arquivo"> <input type="submit" value="submit" name="Busca">
+    <br> <button id="btn"> Enviar </button> <br>
+
+     <br><h2> DOWNLOAD </h2>
+     <h3> Arquivo: </h3> <input id="login" type="url" name="url2">
+     <br> <h3> Baixar para: </h3> <input id="senha" type="" name="url3">
+
+     <button id="btn"> Baixar </button>
+   
+</body>
+</html>
